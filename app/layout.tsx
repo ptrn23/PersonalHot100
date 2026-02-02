5da3042d382547f5a2cb35b9a515c4890a7552fc
ptrn23 <pnecasio23@gmail.com>
2026-02-02 20:18:02 +0800

    feat: add embed link cover

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,22 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Personal Hot 100",
-  description: "The official weekly music chart tracking my most played songs on Last.fm.",
+  description: "The official weekly music chart tracking your most played songs on Last.fm.",
+  openGraph: {
+    title: 'Personal Hot 100',
+    description: "The official weekly music chart tracking your most played songs on Last.fm.",
+    url: 'https://personal-hot-100.vercel.app',
+    siteName: 'Personal Hot 100',
+    images: [
+      {
+        url: '/cover.jpg',
+        width: 2048,
+        height: 1536,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
