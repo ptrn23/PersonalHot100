ca84c680ea109242b1616e66478d390da9af5dff
ptrn23 <pnecasio23@gmail.com>
2026-02-28 10:29:42 +0800

    feat: update for 2026-02-20

--- scripts/process_weekly.py
+++ scripts/process_weekly.py
@@ -75,7 +75,7 @@ def _get_files_to_process(points_dir, specific_week):
 
 def main():
     year = 2026
-    specific_week = "02-13"  # change to None to process all weeks
+    specific_week = "02-20"  # change to None to process all weeks
     
     process_weekly_charts(
         year=year,
